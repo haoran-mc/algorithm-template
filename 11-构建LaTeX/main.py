bc80972282a99ebe4fc0b8db0575205e06b4171d
haoran <haoran.mc@outlook.com>
2021-09-24 15:45:58 +0800

    :bug:no indent paragraph and no empty dir

--- 11-构建LaTeX/main.py
+++ 11-构建LaTeX/main.py
@@ -1,9 +1,11 @@
 import os
 
+
 fileignore = [
-    ".git", ".font", "01-STL", "11-构建LaTeX", "in", "00-封装类",
-    "in.txt", "out.txt", ".ogmc", "o-o", ".gitignore"
-]
+        ".git", ".font", "00-测试", "11-构建LaTeX", "in",
+        "in.txt", "out.txt", ".ogmc", "o-o", ".gitignore", "README.md"
+        ]
+
 
 # 向要生成的模板写入准备好的头部与尾部
 def writetex(filename):
@@ -17,39 +19,38 @@ def writetex(filename):
 
 
 def getLaTeX(path, depth):
-    files = os.listdir(path)                        # 得到文件夹下的所有文件(包括文件夹)名称
-    files.sort()                                    # 排序
-    for file in files:                              # 遍历当前文件夹下所有文件
-        if os.path.isdir(path + file):              # 如果是文件夹，递归
-            if file in fileignore:                  # 排除无关文件夹
+    files = os.listdir(path)                                    # 得到文件夹下的所有文件(包括文件夹)名称
+    files.sort()                                                # 排序
+    for file in files:                                          # 遍历当前文件夹下所有文件
+        # 除去文件的数字前缀
+        if len(file) > 3 and file[2] == "-":
+            fileNameWithoutNum = file[3: len(file)]
+        else:
+            fileNameWithoutNum = file
+        if os.path.isdir(path + file):                          # 如果是文件夹，递归；如果是文件，写入.tex
+            if file in fileignore:                              # 排除无关文件夹
                 continue
-            if depth == 0:                          # 一级目录特判
-                tex.write("\\section{" + file + "}\n")
+            if depth == 3:                                      # 四级及以上的目录直接过滤
+                continue
+            if not os.listdir(path + file):                     # 空文件夹
+                continue
             else:
-                str = "sub"
-                for i in range(1, depth):
-                    str += "sub"
-                if (len(str) > 6):                  # 三级目录特判
-                    tex.write("\\paragraph{" + file + "}\n")
-                    continue
-                tex.write("\\" + str + "section{" + file + "}\n")
-            getLaTeX(path + file + "/", depth + 1)  # 递归寻找下一级目录
-        elif os.path.isfile(path + file):           # 如果是文件
-            if file in fileignore:                  # 排除无关文件夹
+                str = "sub" * depth
+                str += "section"                                # 根据depth生成相应等级的目录
+                tex.write("\\" + str + "{" + fileNameWithoutNum + "}\n")
+                getLaTeX(path + file + "/", depth + 1)          # 递归寻找下一级目录
+        elif os.path.isfile(path + file):                       # 如果是文件
+            if file in fileignore:                              # 排除无关文件
                 continue
-            #  if depth == 1:
-            #      str = "sub"
-            #  elif depth == 2:
-            #      str = "subsub"
-            str = "sub"
-            for i in range(1, depth):
-                str += "sub"
-            if (len(str) > 6):
-                tex.write("\\paragraph{" + file + "}\n")
+            if depth == 3:                                      # 四级文件使用\paragraph
+                tex.write("\\paragraph{" + fileNameWithoutNum + "}\n")
+                tex.write("\\noindent\n")
                 tex.write("\\lstinputlisting{" + "." + path + file + "}\n")
-                continue
-            tex.write("\\" + str + "section{" + file + "}\n")
-            tex.write("\\lstinputlisting{" + "." + path + file + "}\n")
+            else:
+                str = "sub" * depth
+                str += "section"
+                tex.write("\\" + str + "{" + fileNameWithoutNum + "}\n")
+                tex.write("\\lstinputlisting{" + "." + path + file + "}\n")
 
 
 os.chdir("..")                                      # 设置路径为..
