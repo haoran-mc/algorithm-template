f2f0d43720893ec4c19a1b3b98b2b565682cdd9e
haoran <haoran.mc@outlook.com>
2022-11-22 11:50:38 +0800

    debian -> mac

--- 01-语言基础/01-封装/00-头文件.cpp
+++ 01-语言基础/01-封装/00-头文件.cpp
@@ -19,38 +19,38 @@ typedef pair<int, PII> PIII;
 // const int maxn = <++>;
 const int INF = 0x3f3f3f3f;
 const ll INF_LL = 0x3f3f3f3f3f3f3f3fLL;
-// 上 右 下 左 上左 上右 下右 下左
-const int dx[] = {‐1, 0, 1, 0, ‐1, ‐1, 1, 1};
-const int dy[] = {0, 1, 0, ‐1, ‐1, 1, 1, ‐1};
-const double PI = acos(‐1.0);
-const double eps = 1e‐6;
+// 上右 下左 上左 上右 下右 下左
+const int dx[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int dy[] = {0, 1, 0, -1, -1, 1, 1, -1};
+const double PI = acos(-1.0);
+const double eps = 1e-6;
 const double gold = (1 + sqrt(5)) / 2; //黄金分割 = 1.61803398...
 const int FACT[] = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880};
 priority_queue<int, vector<int>, less<int>> he; /*默认是less，即数字大的优先级高*/
 priority_queue<int, vector<int>, greater<int>> hE; /*默认是less，即数字大的优先级高*/
 
 void clear(queue<int>& q) {
-queue<int> empty;
-swap(empty, q);
+    queue<int> empty;
+    swap(empty, q);
 }
 
-inline int sigma(char c) {return c ‐ 'a';};
+inline int sigma(char c) {return c - 'a';};
 
 inline int sgn(double x) { // 判断x是否等于0
-if (fabs(x) < eps) return 0;
-else return x < 0 ? ‐1 : 1;
+    if (fabs(x) < eps) return 0;
+    else return x < 0 ? -1 : 1;
 }
 
 inline int dcmp(double x, double y) { //比较两个浮点数：0 相等；‐1 小于；1 大于
-if (fabs(x ‐ y) < eps) return 0;
-else return x < y ? ‐1 : 1;
+    if (fabs(x - y) < eps) return 0;
+    else return x < y ? -1 : 1;
 }
 
 struct Point {
-double x, y;
-Point() {}
-Point(double _x, double _y): x(_x), y(_y) {}
-bool operator == (const Point _point) {return sgn(x ‐ _point.x) == 0 && sgn(y ‐ _point.y) == 0;}
+    double x, y;
+    Point() {}
+    Point(double _x, double _y): x(_x), y(_y) {}
+    bool operator == (const Point _point) {return sgn(x ‐ _point.x) == 0 && sgn(y ‐ _point.y) == 0;}
 };
 
 inline double dis(Point p1, Point p2) {return hypot(p1.x‐p2.x, p1.y‐p2.y);}
@@ -59,28 +59,28 @@ template <typename T>
 T Smax(T x) {return x;}
 template<typename T, typename... Args>
 T Smax(T a, Args... args) {
-return _max(a, Smax(args...));
+    return _max(a, Smax(args...));
 }
 
 template <typename T>
 T Smin(T x) {return x;}
 template<typename T, typename... Args>
 T Smin(T a, Args... args) {
-return _min(a, Smin(args...));
+    return _min(a, Smin(args...));
 }
 
 void solve() {
-;//<++>
+    ;//<++>
 }
 
 int main() {
-// signed main() {
+    // signed main() {
 #ifndef ONLINE_JUDGE
-// freopen("in.txt", "r", stdin);
-// freopen("out.txt", "w", stdout);
+    // freopen("in.txt", "r", stdin);
+    // freopen("out.txt", "w", stdout);
 #endif
-// ios::sync_with_stdio(false);
-// cin.tie(NULL), cout.tie(NULL);
-solve();
-return 0;
+    // ios::sync_with_stdio(false);
+    // cin.tie(NULL), cout.tie(NULL);
+    solve();
+    return 0;
 }
---
+++ 07-数据结构/04-哈希表/hashmap.cpp
@@ -0,0 +1,6 @@
+//    filename: hashmap.cpp
+//      author: Haoran Liu
+//    datetime: 2022-09-24 18:09 Sat
+//      source:
+//   algorithm: hashmap
+// description:
